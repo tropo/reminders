130a808fe432cc24484f7c20495954d9d98f9bba
Chris Matthieu <chris@matthieu.us>
2011-02-25 18:57:13 -0700

    refactored the API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,40 +5,43 @@ class ApiController < ApplicationController
     
     @reminders.each do |reminder| 
            
-     if reminder.appointment-1.hour < Time.now.utc  
-                
-       # Send SMS
-       RestClient.get 'http://api.tropo.com/1.0/sessions', {:params => {
-         :action => 'create', 
-         :token => '848b6b17c6229844827847b381a4a7e25c72d0750ec751bc0d10072e58eaa12683b6ea0a8aa5e166ee7bfcc8', 
-         :phonenumber => formatphone(reminder.phonenumber), 
-         :remindermessage => reminder.message + ' at ' + reminder.appointment}}
-
-      else
-
-        # Place outbound reminder call
+      if reminder.appointment-1.hour < Time.now.utc && reminder.flag3.nil?
+        
+        # Send SMS
         RestClient.get 'http://api.tropo.com/1.0/sessions', {:params => {
           :action => 'create', 
-          :token => '3d5eed33429706408efcc0e92307b044ba2ecb3c2e641f69f9179b54e9d04af908e583112195de9ec7b05b9e', 
+          :token => '848b6b17c6229844827847b381a4a7e25c72d0750ec751bc0d10072e58eaa12683b6ea0a8aa5e166ee7bfcc8', 
           :phonenumber => formatphone(reminder.phonenumber), 
-          :remindermessage => reminder.message + ' at ' + reminder.appointment}}
-      
-      end 
-
-      # Write flag
-      if reminder.appointment-1.hour < Time.now.utc 
+          :remindermessage => reminder.message + ' at ' + reminder.appointment.to_s}}
+        
         # Write Flag3
         @reminder = Reminder.find(reminder.id)
         @reminder.flag3 = true
         @reminder.save
         
-      elsif reminder.appointment-1.day < Time.now.utc 
+      elsif reminder.appointment-1.day < Time.now.utc  && reminder.flag2.nil?
+        
+        # Place outbound reminder call
+        RestClient.get 'http://api.tropo.com/1.0/sessions', {:params => {
+          :action => 'create', 
+          :token => '3d5eed33429706408efcc0e92307b044ba2ecb3c2e641f69f9179b54e9d04af908e583112195de9ec7b05b9e', 
+          :phonenumber => formatphone(reminder.phonenumber), 
+          :remindermessage => reminder.message + ' at ' + reminder.appointment.to_s}}
+        
         # Write Flag2
         @reminder = Reminder.find(reminder.id)
         @reminder.flag2 = true
         @reminder.save
         
-      elsif reminder.appointment-1.week < Time.now.utc 
+      elsif reminder.appointment-1.week < Time.now.utc && reminder.flag1.nil?
+        
+        # Place outbound reminder call
+        RestClient.get 'http://api.tropo.com/1.0/sessions', {:params => {
+          :action => 'create', 
+          :token => '3d5eed33429706408efcc0e92307b044ba2ecb3c2e641f69f9179b54e9d04af908e583112195de9ec7b05b9e', 
+          :phonenumber => formatphone(reminder.phonenumber), 
+          :remindermessage => reminder.message + ' at ' + reminder.appointment.to_s}}
+        
         # Write Flag1
         @reminder = Reminder.find(reminder.id)
         @reminder.flag1 = true
